1bfb0133738768a0618d3153fe644c272e48eaa9
Sergey Chikunov <84094895+hapikus@users.noreply.github.com>
2023-08-06 10:32:43 +0300

    feat: add router, controller

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
+const router = require('./router/index');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 // middleware
-
-app.use(express.json);
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use('/api', router);
 
 const start = async () => {
   try {
