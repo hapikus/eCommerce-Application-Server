ce529880107b54bfd580e994c1d6f369ccdc7882
Sergey Chikunov <84094895+hapikus@users.noreply.github.com>
2023-08-06 09:20:39 +0300

    feat: add based middleware, add db

---
+++ index.js
@@ -0,0 +1,32 @@
+// MongoDB:
+// hulahap
+// sfhX9wc3T3fWZ2XV
+
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+
+app.use(express.json);
+app.use(cookieParser());
+app.use(cors());
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    app.listen(5000, () => console.log(`Server started on PORT = ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
